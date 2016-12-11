8324a29ca3fd73d27c54e3a2c61cf95e736b2fc6
Khanh Trung Tran <khanhtrung90@yahoo.com>
2016-12-12 00:34:15 +0700

    First commit

---
+++ ConcurrencyDemo/ViewController.swift
@@ -0,0 +1,228 @@
+//
+//  ViewController.swift
+//  ConcurrencyDemo
+//
+//  Created by Hossam Ghareeb on 11/15/15.
+//  Copyright © 2015 Hossam Ghareeb. All rights reserved.
+//
+
+import UIKit
+
+let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg",
+                 "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg",
+                 "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg",
+                 "https://captbbrucato.files.wordpress.com/2011/08/dscf0585_stitch-besonhurst-2.jpg"]
+
+class Downloader {
+    
+    class func downloadImageWithURL(_ url:String) -> UIImage! {
+        
+        let data = try? Data(contentsOf: URL(string: url)!)
+        return UIImage(data: data!)
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    @IBOutlet weak var imageView4: UIImageView!
+    
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    
+    var operationQueue : OperationQueue!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didClickOnStart(_ sender: AnyObject) {
+        
+        // MARK: Basic
+        /*
+         let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+         self.imageView1.image = img1
+         
+         let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+         self.imageView2.image = img2
+         
+         let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+         self.imageView3.image = img3
+         
+         let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+         self.imageView4.image = img4
+         */
+        
+        
+        // MARK: Using Concurrent Dispatch Queues
+        /*let concurrentQueue = DispatchQueue.global()
+         concurrentQueue.async {
+         let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+         DispatchQueue.main.async {
+         self.imageView1.image = img1
+         }
+         }
+         
+         concurrentQueue.async {
+         let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+         DispatchQueue.main.async {
+         self.imageView2.image = img2
+         }
+         }
+         
+         concurrentQueue.async {
+         let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+         DispatchQueue.main.async {
+         self.imageView3.image = img3
+         }
+         }
+         
+         concurrentQueue.async {
+         let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+         DispatchQueue.main.async {
+         self.imageView4.image = img4
+         }
+         }
+         */
+        
+        // MARK: Using Serial Dispatch Queues
+        /*
+         let serialQueue = DispatchQueue(label: "com.appcoda.imagesQueue")
+         serialQueue.async {
+         let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+         DispatchQueue.main.async {
+         self.imageView1.image = img1
+         }
+         }
+         
+         serialQueue.async {
+         let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+         DispatchQueue.main.async {
+         self.imageView2.image = img2
+         }
+         }
+         
+         serialQueue.async {
+         let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+         DispatchQueue.main.async {
+         self.imageView3.image = img3
+         }
+         }
+         
+         serialQueue.async {
+         let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+         DispatchQueue.main.async {
+         self.imageView4.image = img4
+         }
+         }
+         */
+        
+        
+        // MARK: Using Operation Queues
+        /*
+         let operationQueue = OperationQueue()
+         operationQueue.addOperation {
+         let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+         OperationQueue.main.addOperation({
+         self.imageView1.image = img1
+         })
+         }
+         
+         operationQueue.addOperation {
+         let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+         OperationQueue.main.addOperation({
+         self.imageView2.image = img2
+         })
+         }
+         
+         operationQueue.addOperation {
+         let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+         OperationQueue.main.addOperation({
+         self.imageView3.image = img3
+         })
+         }
+         
+         operationQueue.addOperation {
+         let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+         OperationQueue.main.addOperation({
+         self.imageView4.image = img4
+         })
+         }
+         */
+        
+        // MARK: Using Block Operation Queues
+        operationQueue = OperationQueue()
+        
+        // operation1
+        let operation1 = BlockOperation(block: {
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+            OperationQueue.main.addOperation({
+                self.imageView1.image = img1
+            })
+        })
+        operation1.completionBlock = {
+            print("Operation 1 completed, cancelled:\(operation1.isCancelled) ")
+        }
+        operationQueue.addOperation(operation1)
+        
+        // operation2
+        let operation2 = BlockOperation(block: {
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            OperationQueue.main.addOperation({
+                self.imageView2.image = img2
+            })
+        })
+        operation2.addDependency(operation1)
+        operation2.completionBlock = {
+            print("Operation 2 completed, cancelled:\(operation2.isCancelled) ")
+        }
+        operationQueue.addOperation(operation2)
+        
+        // operation3
+        let operation3 = BlockOperation(block: {
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            OperationQueue.main.addOperation({
+                self.imageView3.image = img3
+            })
+        })
+        operation3.addDependency(operation2)
+        operation3.completionBlock = {
+            print("Operation 3 completed, cancelled:\(operation3.isCancelled) ")
+        }
+        operationQueue.addOperation(operation3)
+        
+        // operation4
+        let operation4 = BlockOperation(block: {
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            OperationQueue.main.addOperation({
+                self.imageView4.image = img4
+            })
+        })
+        operation4.completionBlock = {
+            print("Operation 4 completed, cancelled:\(operation4.isCancelled) ")
+        }
+        operationQueue.addOperation(operation4)
+    }
+    
+    @IBAction func didClickOnCancel(_ sender: Any) {
+        self.operationQueue.cancelAllOperations()
+    }
+    
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        self.sliderValueLabel.text = "\(sender.value * 100.0)"
+    }
+    
+}
+
